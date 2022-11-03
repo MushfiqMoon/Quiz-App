6fdbaecff923656228af87a2ff9a851dc8b69e64
MushfiqMoon <mushfiq.moon013@gmail.com>
2022-11-03 16:07:54 +0600

    initial project setup done

---
+++ src/routes/router.js
@@ -0,0 +1,31 @@
+import {
+    createBrowserRouter,
+} from "react-router-dom";
+import { ErrorPage } from "../components/pages/ErrorPage";
+import Home from "../components/pages/Home";
+import Login from "../components/pages/Login";
+import Register from "../components/pages/Register";
+import Main from "../layouts/Main";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/register",
+                element: <Register />,
+            },
+        ],
+
+    },
+]);
